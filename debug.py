594ab11f77f791d0a570110aeadc052a76cd5598
baptistedfr <bapdufo@gmail.com>
2025-01-06 23:29:40 +0100

    update

--- debug.py
+++ debug.py
@@ -7,7 +7,7 @@ freq = "daily"
 model = LPPLS
 
 fw = Framework(freq, model)
-optimizers = [NELDER_MEAD]
+optimizers = [NELDER_MEAD(model)]
 dates_sets = {
     "Set 1": ("01/04/2003", "02/01/2008"),
     "Set 2": ("01/02/2007", "01/02/2011"),
@@ -15,17 +15,17 @@ dates_sets = {
 }
 
 for optimizer in optimizers:
-    print(f"Running process for {optimizer.__name__}\n")
+    print(f"Running process for {optimizer.__class__.__name__}\n")
     for set_name, (start_date, end_date) in dates_sets.items():
         print(f"Running process for {set_name} from {start_date} to {end_date}")
-        fw.process(start_date, end_date, optimizer)
+        results = fw.process(start_date, end_date, optimizer)
         # On peut save en json nos résultats pour les réutiliser plus tard
         start_date_obj = datetime.strptime(start_date, "%d/%m/%Y")
         end_date_obj = datetime.strptime(end_date, "%d/%m/%Y")
-        filename = f"results/{optimizer.__name__}/{model.__name__}-results_{freq}_{start_date_obj.strftime('%d-%m-%y')} {end_date_obj.strftime('%d-%m-%y')}_{optimizer.__name__}.json"
+        filename = f"results/{optimizer.__class__.__name__}/-results_{freq}_{start_date_obj.strftime('%d-%m-%y')} {end_date_obj.strftime('%d-%m-%y')}.json"
         # Sauvegarder les résultats
-        fw.save_results(fw.results, filename)
+        fw.save_results(results, filename)
         # On check la significativité des résultats
-        fw.analyze()
+        best_results = fw.analyze(results)
         # On peut visualiser les résultats finaux
-        fw.visualize()
+        fw.visualize(best_results)
