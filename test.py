f443854b8988bb29b0be2ad70cc02349cf86f4d6
baptistedfr <bapdufo@gmail.com>
2025-01-10 16:38:37 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 from GQLib.Framework import Framework
 from GQLib.Optimizers import MPGA, PSO, SGA, SA, NELDER_MEAD, TABU
+<<<<<<< Updated upstream
 from GQLib.Models import LPPL, LPPLS
 
 fw_daily = Framework("daily", lppl_model=LPPL)
 fw_daily.generate_all_rectangle(optimizers = [SA(), SGA(), NELDER_MEAD(), TABU()] , rerun=True, save=True)
 # fw_weekly = Framework("weekly", lppl_model=LPPL)
 # fw_weekly.generate_all_rectangle(optimizers = [SA(), SGA(), NELDER_MEAD(), TABU()] , rerun=True, save=True)
+=======
+fw_daily = Framework("daily")
+fw_daily.generate_all_rectangle(optimizers = [SA(), SGA(), NELDER_MEAD(), TABU(), PSO(), MPGA()] , rerun=True, save=True)
+fw_weekly = Framework("weekly")
+fw_weekly.generate_all_rectangle(optimizers = [SA(), SGA(), NELDER_MEAD(), TABU(), PSO(), MPGA()] , rerun=True, save=True)
+>>>>>>> Stashed changes
 # best_results = fw.analyze(result_json_name="results/SGA/daily/04-2003 01-2008.json")
 # best_results_2 = fw.analyze(result_json_name="results/PSO/daily/04-2003 01-2008.json")
 # best_results_3 = fw.analyze(result_json_name="results/MPGA/daily/04-2003 01-2008.json")
