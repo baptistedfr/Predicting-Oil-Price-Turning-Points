d92b454d5d133831ce4d5f72214ecc6773c3e30c
baptistedfr <bapdufo@gmail.com>
2025-01-12 22:48:15 +0100

    Add USO

--- test.py
+++ test.py
@@ -10,71 +10,147 @@ import numpy as np
 
 wti = AssetProcessor(input_type = InputType.WTI)
 
-wti.generate_all_rectangle(frequency = "daily",
-                        optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
-                        significativity_tc=0.3,
-                        rerun = True,
-                        nb_tc = 10,
-                        save=True,
-                        save_plot=True)
+try:
+    wti.generate_all_rectangle(frequency = "daily",
+                            optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
+                            significativity_tc=0.3,
+                            rerun = True,
+                            nb_tc = 10,
+                            save=True,
+                            save_plot=True)
+except:
+    pass
 
-wti.generate_all_rectangle(frequency = "daily",
+try:
+    wti.generate_all_rectangle(frequency = "daily",
+                            optimizers =  [SA(LPPLS), SGA(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS), MPGA(LPPLS), PSO(LPPLS)], 
+                            significativity_tc=0.3,
+                            rerun = True,
+                            nb_tc = 10,
+                            save=True,
+                            save_plot=True)
+except:
+    pass
+
+try:
+    wti.generate_all_rectangle(frequency = "weekly",
+                            optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
+                            significativity_tc=0.3,
+                            rerun = True,
+                            nb_tc = 10,
+                            save=True,
+                            save_plot=True)
+except:
+    pass
+
+try:
+    wti.generate_all_rectangle(frequency = "weekly",
                         optimizers =  [SA(LPPLS), SGA(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS), MPGA(LPPLS), PSO(LPPLS)], 
                         significativity_tc=0.3,
                         rerun = True,
                         nb_tc = 10,
                         save=True,
                         save_plot=True)
-
-wti.generate_all_rectangle(frequency = "weekly",
-                        optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
-                        significativity_tc=0.3,
-                        rerun = True,
-                        nb_tc = 10,
-                        save=True,
-                        save_plot=True)
-
-wti.generate_all_rectangle(frequency = "weekly",
-                        optimizers =  [SA(LPPLS), SGA(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS), MPGA(LPPLS), PSO(LPPLS)], 
-                        significativity_tc=0.3,
-                        rerun = True,
-                        nb_tc = 10,
-                        save=True,
-                        save_plot=True)
+except:
+    pass
 
 sp500 = AssetProcessor(input_type = InputType.SP500)
 
-sp500.generate_all_rectangle(frequency = "daily",
+try:
+    sp500.generate_all_rectangle(frequency = "daily",
                         optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
                         significativity_tc=0.3,
                         rerun = True,
                         nb_tc = 10,
                         save=True,
                         save_plot=True)
+except:
+    pass
 
-sp500.generate_all_rectangle(frequency = "daily",
+try:
+    sp500.generate_all_rectangle(frequency = "daily",
                         optimizers =  [SA(LPPLS), SGA(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS), MPGA(LPPLS), PSO(LPPLS)], 
                         significativity_tc=0.3,
                         rerun = True,
                         nb_tc = 10,
                         save=True,
                         save_plot=True)
+except:
+    pass
 
-# wti.generate_all_rectangle(frequency = "daily",
-#                         optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
-#                         significativity_tc=0.15,
-#                         rerun = False,
-#                         nb_tc = 15,
-#                         save=False,
-#                         save_plot=False)
+try:
+    sse = AssetProcessor(input_type = InputType.SSE)
+except:
+    pass
+
+try:
+    sse.generate_all_rectangle(frequency = "daily",
+                        optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
+                        significativity_tc=0.3,
+                        rerun = True,
+                        nb_tc = 10,
+                        save=True,
+                        save_plot=True)
+except:
+    pass
+
+try:
+    sse.generate_all_rectangle(frequency = "daily",
+                        optimizers =  [SA(LPPLS), SGA(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS), MPGA(LPPLS), PSO(LPPLS)], 
+                        significativity_tc=0.3,
+                        rerun = True,
+                        nb_tc = 10,
+                        save=True,
+                        save_plot=True)
+except:
+    pass
 
 
-# sp500 = AssetProcessor(input_type = InputType.SP500)
-# sp500.generate_all_rectangle(frequency = "daily",
-#                         optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS)], 
-#                         significativity_tc=0.3,
-#                         rerun = True,
-#                         nb_tc = 20,
-#                         save=True,
-#                         save_plot=True)
+try:
+    uso = AssetProcessor(input_type = InputType.USO)
+except:
+    pass
 
+try:
+    uso.generate_all_rectangle(frequency = "daily",
+                        optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
+                        significativity_tc=0.3,
+                        rerun = True,
+                        nb_tc = 10,
+                        save=True,
+                        save_plot=True)
+except:
+    pass
+
+try:
+    uso.generate_all_rectangle(frequency = "daily",
+                        optimizers =  [SA(LPPLS), SGA(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS), MPGA(LPPLS), PSO(LPPLS)], 
+                        significativity_tc=0.3,
+                        rerun = True,
+                        nb_tc = 10,
+                        save=True,
+                        save_plot=True)
+except:
+    pass
+
+try:
+    uso.generate_all_rectangle(frequency = "weekly",
+                            optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
+                            significativity_tc=0.3,
+                            rerun = True,
+                            nb_tc = 10,
+                            save=True,
+                            save_plot=True)
+except:
+    pass
+
+try:
+    uso.generate_all_rectangle(frequency = "weekly",
+                        optimizers =  [SA(LPPLS), SGA(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS), MPGA(LPPLS), PSO(LPPLS)], 
+                        significativity_tc=0.3,
+                        rerun = True,
+                        nb_tc = 10,
+                        save=True,
+                        save_plot=True)
+except:
+    pass
