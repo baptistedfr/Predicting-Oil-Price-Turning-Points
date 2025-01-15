d514d6cf95538b3e5090ae76ef7b11c1ae244476
aremiat <al.rem@outlook.fr>
2025-01-15 13:55:48 +0100

    add the latex paper, ran results

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from GQLib.Framework import Framework
-from GQLib.Optimizers import MPGA, PSO, SGA, SA, NELDER_MEAD, TABU, FA
+from GQLib.Optimizers import MPGA, PSO, SGA, SA, NELDER_MEAD, TABU, FA, MCMC
 from GQLib.Models import LPPL, LPPLS
 from GQLib.enums import InputType
 from GQLib.AssetProcessor import AssetProcessor
@@ -8,19 +8,19 @@ import numpy as np
 
 
 
-# wti = AssetProcessor(input_type = InputType.WTI)
+wti = AssetProcessor(input_type = InputType.WTI)
 
-# try:
-#     wti.generate_all_rectangle(frequency = "daily",
-#                             optimizers =  [SA(LPPL), SGA(LPPL), NELDER_MEAD(LPPLS), TABU(LPPL), FA(LPPL), MPGA(LPPL), PSO(LPPL)], 
-#                             significativity_tc=0.3,
-#                             rerun = False,
-#                             nb_tc = 10,
-#                             save=False,
-#                             save_plot=False)
-# except:
-#     pass
-
+try:
+    wti.generate_all_rectangle(frequency = "weekly",
+                            optimizers =  [SA(LPPLS), SGA(LPPLS), MPGA(LPPLS), PSO(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS)],
+                            significativity_tc=0.3,
+                            rerun = False,
+                            nb_tc = 10,
+                            save=False,
+                            save_plot=False)
+except:
+    pass
+print("done")
 # try:
 #     wti.generate_all_rectangle(frequency = "daily",
 #                             optimizers =  [SA(LPPLS), SGA(LPPLS), NELDER_MEAD(LPPLS), TABU(LPPLS), FA(LPPLS), MPGA(LPPLS), PSO(LPPLS)], 
