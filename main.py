2638e19c92ca063a4516c921b70a255a32613887
Caymaar <julesmourgues@gmail.com>
2025-05-18 14:35:42 +0200

    GU

--- main.py
+++ main.py
@@ -11,24 +11,7 @@ framework = MiaouFramework(
     data_names=[DataName.SP500],
     set_dates={
         "SP500": {
-            "SP500 period" : ("2000-01-01", "2025-01-01"),
-        }
-    },
-    frequency=1,
-    optimizer=NELDER_MEAD(LPPLS),
-    filtering_method=LPPLSConfidence,
-    subinterval_method=SubIntervalMethod.DIDOU,
-    window_length=1000,
-    optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
-)
-framework.run()
-framework.visualize(save=True)
-
-framework = MiaouFramework(
-    data_names=[DataName.SP500],
-    set_dates={
-        "SP500": {
-            "SP500 period" : ("2000-01-01", "2025-01-01"),
+            "SP500 period" : ("2015-01-01", "2017-01-01"),
         }
     },
     frequency=1,
@@ -41,36 +24,53 @@ framework = MiaouFramework(
 framework.run()
 framework.visualize(save=True)
 
-framework = MiaouFramework(
-    data_names=[DataName.WTI],
-    set_dates={
-        "WTI": {
-            "WTI period" : ("2000-01-01", "2025-01-01"),
-        }
-    },
-    frequency=1,
-    optimizer=NELDER_MEAD(LPPLS),
-    filtering_method=LPPLSConfidence,
-    subinterval_method=SubIntervalMethod.DIDOU,
-    window_length=1000,
-    optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
-)
-framework.run()
-framework.visualize(save=True)
+# framework = MiaouFramework(
+#     data_names=[DataName.SP500],
+#     set_dates={
+#         "SP500": {
+#             "SP500 period" : ("2000-01-01", "2025-01-01"),
+#         }
+#     },
+#     frequency=1,
+#     optimizer=NELDER_MEAD(LPPLS),
+#     filtering_method=LombFilter,
+#     subinterval_method=SubIntervalMethod.DIDOU,
+#     window_length=1000,
+#     optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
+# )
+# framework.run()
+# framework.visualize(save=True)
 
-framework = MiaouFramework(
-    data_names=[DataName.WTI],
-    set_dates={
-        "WTI": {
-            "WTI period" : ("2000-01-01", "2025-01-01"),
-        }
-    },
-    frequency=1,
-    optimizer=NELDER_MEAD(LPPLS),
-    filtering_method=LombFilter,
-    subinterval_method=SubIntervalMethod.DIDOU,
-    window_length=1000,
-    optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
-)
-framework.run()
-framework.visualize(save=True)
+# framework = MiaouFramework(
+#     data_names=[DataName.WTI],
+#     set_dates={
+#         "WTI": {
+#             "WTI period" : ("2000-01-01", "2025-01-01"),
+#         }
+#     },
+#     frequency=1,
+#     optimizer=NELDER_MEAD(LPPLS),
+#     filtering_method=LPPLSConfidence,
+#     subinterval_method=SubIntervalMethod.DIDOU,
+#     window_length=1000,
+#     optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
+# )
+# framework.run()
+# framework.visualize(save=True)
+
+# framework = MiaouFramework(
+#     data_names=[DataName.WTI],
+#     set_dates={
+#         "WTI": {
+#             "WTI period" : ("2000-01-01", "2025-01-01"),
+#         }
+#     },
+#     frequency=1,
+#     optimizer=NELDER_MEAD(LPPLS),
+#     filtering_method=LombFilter,
+#     subinterval_method=SubIntervalMethod.DIDOU,
+#     window_length=1000,
+#     optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
+# )
+# framework.run()
+# framework.visualize(save=True)
