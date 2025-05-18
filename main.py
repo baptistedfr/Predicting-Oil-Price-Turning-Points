53120ed384e138c209e3932a7c60f050cc660938
Caymaar <julesmourgues@gmail.com>
2025-05-18 10:59:19 +0200

    run sp500

--- main.py
+++ main.py
@@ -5,16 +5,30 @@ from GQLib.filterings import LombFilter, LPPLSConfidence, StationarityFilter
 from GQLib.Models import LPPL, LPPLS
 from GQLib.logging import configure_logger
 
-configure_logger("DEBUG")
+configure_logger("INFO")
 
 framework = MiaouFramework(
-    data_names=[DataName.SP500, DataName.WTI],
+    data_names=[DataName.SP500],
     set_dates={
         "SP500": {
             "SP500 period" : ("2000-01-01", "2025-01-01"),
-        },
-        "WTI": {
-            "WTI period" : ("2000-01-01", "2025-01-01"),
+        }
+    },
+    frequency=1,
+    optimizer=NELDER_MEAD(LPPLS),
+    filtering_method=LPPLSConfidence,
+    subinterval_method=SubIntervalMethod.DIDOU,
+    window_length=1000,
+    optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
+)
+framework.run()
+framework.visualize(save=True)
+
+framework = MiaouFramework(
+    data_names=[DataName.SP500],
+    set_dates={
+        "SP500": {
+            "SP500 period" : ("2000-01-01", "2025-01-01"),
         }
     },
     frequency=1,
@@ -28,11 +42,8 @@ framework.run()
 framework.visualize(save=True)
 
 framework = MiaouFramework(
-    data_names=[DataName.SP500, DataName.WTI],
+    data_names=[DataName.WTI],
     set_dates={
-        "SP500": {
-            "SP500 period" : ("2000-01-01", "2025-01-01"),
-        },
         "WTI": {
             "WTI period" : ("2000-01-01", "2025-01-01"),
         }
@@ -40,6 +51,23 @@ framework = MiaouFramework(
     frequency=1,
     optimizer=NELDER_MEAD(LPPLS),
     filtering_method=LPPLSConfidence,
+    subinterval_method=SubIntervalMethod.DIDOU,
+    window_length=1000,
+    optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
+)
+framework.run()
+framework.visualize(save=True)
+
+framework = MiaouFramework(
+    data_names=[DataName.WTI],
+    set_dates={
+        "WTI": {
+            "WTI period" : ("2000-01-01", "2025-01-01"),
+        }
+    },
+    frequency=1,
+    optimizer=NELDER_MEAD(LPPLS),
+    filtering_method=LombFilter,
     subinterval_method=SubIntervalMethod.DIDOU,
     window_length=1000,
     optimizer_params={'maxiter': 5000, 'maxfev': 15000, 'fatol': 1e-4, 'xatol': 1e-4}
